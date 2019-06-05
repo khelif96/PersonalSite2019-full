da57b8578a649291cdaa89535267b9b6af6e946e
khelif96 <khelif96@gmail.com>
2019-06-04 23:13:08 -0400

    Smooth scrolling and snapping to pages

--- frontend-rd/src/App.js
+++ frontend-rd/src/App.js
@@ -1,12 +1,14 @@
 import React, { PureComponent } from 'react';
 import './App.css';
-import { Home } from './views';
+import { Home, About, Experience } from './views';
 
 class App extends PureComponent {
   render() {
     return (
       <div className="App">
         <Home />
+        <About />
+        <Experience />
       </div>
     );
   }
