027a2a77f5f3c0b6a6c76c5c7b71b584546f7ef7
khelif96 <khelif96@gmail.com>
2019-06-08 16:54:49 -0400

    Project page is now somewhat responsive

--- frontend-rd/src/components/ProjectContainer/ProjectModule/ProjectModule.jsx
+++ frontend-rd/src/components/ProjectContainer/ProjectModule/ProjectModule.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { TextBox } from '../..';
 import styles from './ProjectModule.module.css';
 
 function ProjectModule(props) {
@@ -12,10 +13,13 @@ function ProjectModule(props) {
   return (
     <div className={styles.container}>
       <h2>{title}</h2>
-      <p>{description}</p>
+      <div className={styles.content}>
+        <TextBox className={styles.textbox}>{description}</TextBox>
+      </div>
+
       <div className={styles.links}>
-        {github && <a href={github}>Git</a>}
-        {link && <a href={link}>Link</a>}
+        {github && <a href={github} target="_blank" rel="noopener noreferrer"><i className="fab fa-github" /></a>}
+        {link && <a href={link} target="_blank" rel="noopener noreferrer"><i className="fas fa-link" /></a>}
       </div>
     </div>
   );
