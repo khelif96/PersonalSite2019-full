384d60046f4aaa671c580bc4ea61d6c11f4edce2
khelif96 <khelif96@gmail.com>
2019-06-26 22:42:16 -0400

    Set up apollo

--- frontend-rd/src/App.jsx
+++ frontend-rd/src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo-hooks';
+import ApolloClient from 'apollo-boost';
+
 import './App.css';
 import {
   Home, About, Experience, Projects, Contact,
@@ -6,16 +9,21 @@ import {
 
 import { Footer } from './components';
 
+
 function App() {
+  const client = new ApolloClient({ uri: 'http://localhost:4000/' });
+
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Home />
+        <About />
+        <Experience />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </ApolloProvider>
   );
 }
 
