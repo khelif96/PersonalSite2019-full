8b7a26e3a2850b243b18dfbdf762dcfd42b44c99
khelif96 <khelif96@gmail.com>
2019-06-26 22:41:06 -0400

    Loading Icon

--- frontend-rd/src/components/index.js
+++ frontend-rd/src/components/index.js
@@ -5,7 +5,8 @@ import JobContainer from './JobContainer/JobContainer';
 import ProjectContainer from './ProjectContainer/ProjectContainer';
 import ContactContainer from './ContactContainer/ContactContainer';
 import Footer from './Footer/Footer';
+import LoadingAnimation from './Loading/Loading';
 
 export {
-  Button, Header, TextBox, JobContainer, ProjectContainer, ContactContainer, Footer,
+  Button, Header, TextBox, JobContainer, ProjectContainer, ContactContainer, Footer, LoadingAnimation,
 };
